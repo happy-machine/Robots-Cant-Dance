369246a438f3f5e0c86d5bebf9c00205200f8e7e
Happy Machine <thisisdjfresh@gmail.com>
2018-05-01 21:19:58 +0100

    merging with master

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,14 @@
 class MessagesController < ApplicationController
   def create
-    p params
-    message_params
     @room = Room.find(params[:room_id])
-    @user = User.find(params[:type])
-    @message = @room.messages.create!(content: params[:content], user: @user, room: @room)
-    p @room
-    p @user
-    p @message
+    @message = @room.messages.create!(content: params[:message][:content], user: current_user, room: @room)
+    p @room.id
+    redirect_to room_path(@room)
   end
 
   private
     def message_params
-      params.require(:message).permit(:type, :room_id, :message, :content)
+      params.require(:message).permit(:type, :room_id, [:message][:content])
     end
 
 end
