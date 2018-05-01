85b2e3e431d90ae79a98325871bee76988453f8c
Thomas Callum Brook <thomascallumbrook@gmail.com>
2018-05-01 17:19:50 +0100

    Form broken, trying to fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,18 @@
 class MessagesController < ApplicationController
   def create
+    p params
+    message_params
     @room = Room.find(params[:room_id])
-    @message = @room.messages.new(content: params[:content], user: current_user)
+    @user = User.find(params[:type])
+    @message = @room.messages.create!(content: params[:content], user: @user, room: @room)
+    p @room
+    p @user
+    p @message
   end
+
+  private
+    def message_params
+      params.require(:message).permit(:type, :room_id, :message, :content)
+    end
+
 end
