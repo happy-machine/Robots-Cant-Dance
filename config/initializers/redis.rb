84ce4ece50d83b66c65b3bd125bb77b30aee464d
Happy Machine <thisisdjfresh@gmail.com>
2018-05-30 18:55:50 +0100

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["redis://redistogo:e85bbd9991f38e388b38f686da8f0d94@cod.redistogo.com:10951/"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.redis = REDIS
